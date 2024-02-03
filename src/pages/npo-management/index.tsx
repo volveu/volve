3132bef33b676c98ccae8e6f744e95b7debd91ff
jonasgwt <jongoh2000@gmail.com>
2024-02-03 17:18:54 +0800

    Create basic FE

---
+++ src/pages/npo-management/index.tsx
@@ -0,0 +1,158 @@
+import { PageLayout } from "../../components/Layout";
+import { LoadingPage } from "../../components/Loading";
+import Pagination from "../../components/Pagination";
+import NpoModal from "./npo-modal";
+import React from "react";
+import { api } from "~/utils/api";
+
+const NpoManagement = () => {
+  const ENTRIES_PER_PAGE = 10;
+  const [showModal, setShowModal] = React.useState(false);
+  const [isEdit, setIsEdit] = React.useState(false);
+  const [id, setId] = React.useState<string | null>(null);
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [currPage, setCurrPage] = React.useState(1);
+  const [totalPages, setTotalPages] = React.useState(1);
+
+  const useNPOs = api.npo.getAll.useQuery();
+
+  React.useEffect(() => {
+    if (!useNPOs.data) return;
+    setTotalPages(Math.ceil(useNPOs.data.length / ENTRIES_PER_PAGE));
+  }, [useNPOs.data]);
+
+  const handleEdit = (currId: string) => {
+    setIsEdit(true);
+    setShowModal(true);
+    setId(currId);
+  }
+
+  const handleCreate = () => {
+    setIsEdit(false);
+    setShowModal(true);
+  }
+
+  const setModalAndRefetch = async (open: boolean) => {
+    setShowModal(open);
+    await useNPOs.refetch();
+  }
+
+  return (
+    <PageLayout>
+      <NpoModal showModal={showModal} setShowModal={setModalAndRefetch} isEdit={isEdit} id={id}/>
+      <div className="flex flex-col items-center">
+      <div className="flex flex-col justify-around items-center mt-12">
+        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+      </div>
+      <div className="flex flex-row items-center justify-between w-[90vw] md:w-[50vw] md:max-w-xl m-auto font-bold text-[2rem]">
+        <h1>NPOs</h1>
+        <NewNPOButton onClick={handleCreate} />
+      </div>
+      <div className="flex flex-col items-center min-h-screen gap-[30px] mt-[10px]">
+        {useNPOs.isLoading ? <LoadingPage /> : 
+        useNPOs.data?.
+        filter((npo) => searchTerm ? npo.name.toLowerCase().includes(searchTerm.toLowerCase()): true).
+        slice((currPage - 1) * ENTRIES_PER_PAGE, currPage * ENTRIES_PER_PAGE).
+        map((npo) => (
+          <NPOCard 
+            key={npo.id}
+            title={npo.name} 
+            body={npo.description} 
+            logoLink={npo.logo} 
+            webLink={npo.website} 
+            onEdit={() => handleEdit(npo.id)}
+          />
+        ))}
+        {!useNPOs.isLoading && useNPOs.data?.length === 0 && <p>No NPOs found</p>}
+      </div>
+      <Pagination currPage={currPage} totalPages={totalPages} setCurrPage={setCurrPage}/>
+      </div>
+    </PageLayout>
+    );
+    }
+export default NpoManagement;
+
+const NPOCard = ({ title, body, webLink, logoLink, onEdit}: { title: string, body: string, webLink: string | null, logoLink: string | null, onEdit: () => void}) => {
+  return (
+      <div
+      className="relative flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row w-[90vw] md:w-[50vw]">
+      <img
+        className="h-66 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        src={logoLink ? logoLink : "https://www.gospel360.org/wp-content/uploads/2022/09/placeholder-16.png"}
+        alt="logo-link" />
+    <div className="flex flex-col justify-start p-6 h-full w-full">
+        <h5
+          className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+          {title}
+        </h5>
+      <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200 md:max-h-[15vw] md:min-h-[5vw] overflow-scroll">
+        {body}
+      </p>
+      <div className="flex flex-row justify-between">
+        <ViewOppButton />
+        {webLink && <VisitNPOButton webLink={webLink} />}
+      </div>
+      <button 
+        type="button" 
+        onClick={onEdit}
+        className="rounded-full flex flex-shrink-0 justify-center items-center gap-1 h-[2.375rem] w-[2.375rem] text-sm font-semibold border border-transparent bg-yellow-500 text-white hover:bg-yellow-600 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 absolute top-4 right-4">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+          <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+          <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+        </svg>
+      </button>
+    </div>
+  </div>
+  )
+}
+
+const NewNPOButton = ({onClick}: {onClick?: () => void}) => {
+  return (
+    <>
+        <button type="button" onClick={onClick} className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-teal-500 text-white hover:bg-teal-600 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle-fill mr-[5px]" viewBox="0 0 20 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
+        </svg>
+            Create New
+        </button>
+      </>
+  )
+}
+
+const VisitNPOButton = ({webLink}: {webLink: string}) => {
+  return (
+    <button type="button" onClick={() => window.open(webLink, "_blank")} className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+      Visit NPO
+      <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+      </svg>
+    </button>
+  )
+}
+
+const ViewOppButton = () => {
+  return (
+    <button type="button" className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+      </svg>
+      View Events
+    </button>
+  )
+}
+
+const SearchBar = ({searchTerm, setSearchTerm}: {searchTerm: string, setSearchTerm: (searchTerm: string) => void}) => {
+  return (
+    <div className="mb-[40px] w-[90vw] md:w-[50vw] md:max-w-xl">   
+        <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+        <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                </svg>
+            </div>
+            <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." required />
+        </div>
+    </div>
+  )
+}
---
+++ src/pages/npo-management/npo-modal.tsx
@@ -0,0 +1,158 @@
+import React from "react";
+import { api } from "../../utils/api";
+import toast from "react-hot-toast";
+
+interface NpoModalPropsBase {
+  showModal: boolean;
+  setShowModal: (show: boolean) => void;
+}
+
+interface NpoModalPropsEdit extends NpoModalPropsBase {
+  isEdit: boolean;
+  id: string;
+}
+
+interface NpoModalPropsCreate extends NpoModalPropsBase {
+  isEdit: boolean;
+  id: null;
+}
+
+type NpoModalProps = NpoModalPropsEdit | NpoModalPropsCreate;
+
+const NpoModal: React.FC<NpoModalProps> = ({showModal, setShowModal, isEdit, id}) => {
+    const useGetNPO = api.npo.get.useQuery({ id: id ?? '' }, {enabled: showModal && isEdit});
+    const [isLoading, setIsLoading] = React.useState(false);
+    const [name, setName] = React.useState("");
+    const [description, setDescription] = React.useState("");
+    const [webLink, setWebLink] = React.useState("");
+    const [logoLink, setLogoLink] = React.useState("");
+    React.useEffect(() => {
+      if(isEdit && useGetNPO.data) {
+        setName(useGetNPO.data.name);
+        setDescription(useGetNPO.data.description);
+        setWebLink(useGetNPO.data.website ?? "");
+        setLogoLink(useGetNPO.data.logo ?? "");
+      } else {
+        setName("");
+        setDescription("");
+        setWebLink("");
+        setLogoLink("");
+      }
+    }, [isEdit, useGetNPO.data]);
+
+    const createNPO = api.npo.create.useMutation({ 
+      onError: () => {
+        toast.error("Failed to create NPO");
+        setIsLoading(false);
+      }, 
+      onSuccess: async () => {
+        toast.success("NPO created successfully");
+        handleClose();
+        setIsLoading(false);
+      } 
+    });
+    const editNPO = api.npo.update.useMutation({ 
+      onError: () => {
+        toast.error("Failed to update NPO");
+        setIsLoading(false);
+      }, 
+      onSuccess: async () => {
+        toast.success("NPO updated successfully");
+        handleClose();
+        setIsLoading(false);
+      } 
+    });
+    const handleClose = () => {
+      setShowModal(false);
+    }
+
+    const handleSubmit = () => {
+      setIsLoading(true);
+      // check if required fields are filled
+      if(!name || !description) {
+        toast.error("Name and Description are required fields!");
+        setIsLoading(false);
+        return;
+      }
+      if(isEdit && id) {
+        editNPO.mutate({ id: id, name, description, website: webLink, logo: logoLink });
+      } else {
+        createNPO.mutate({ name, description, website: webLink, logo: logoLink });
+      }
+    }
+
+  return (
+    <>
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none backdrop-filter backdrop-blur-sm"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl [box-shadow:10px_10px_62px_-5px_rgba(0,0,0,0.75)]">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none dark:bg-gray-700 w-[90vw] md:w-[50vw] md:max-w-xl">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    {isEdit ? "Edit NPO" : "Create NPO"}
+                  </h3>
+                </div>
+                {/*body*/}
+                <div className="p-4 md:p-5">
+                    <div className="grid gap-4 mb-4 grid-cols-2">
+                        <div className="col-span-2">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                            <input type="text" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Name" required />
+                        </div>
+                        <div className="col-span-2">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                            <textarea id="description" maxLength={191} value={description} onChange={(e) => setDescription(e.target.value)}  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write NPO description here"></textarea>                    
+                        </div>
+                        <div className="col-span-2">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Website Link</label>
+                            <input name="website-link" value={webLink} onChange={(e) => setWebLink(e.target.value)}  id="website-link" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="https://example.com" required />
+                        </div>
+                        <div className="col-span-2">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Logo Link</label>
+                            <input name="logo-link" value={logoLink} onChange={(e) => setLogoLink(e.target.value)}  id="logo-link" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="https://example.com" required />
+                        </div>
+                    </div>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={handleClose}
+                  >
+                    Close
+                  </button>
+                  {!isLoading? 
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={handleSubmit}
+                    >
+                    Save Changes
+                </button> 
+                : 
+                  <button disabled type="button" className="py-2.5 px-5 me-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center">
+                    <svg aria-hidden="true" role="status" className="inline w-4 h-4 me-3 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2"/>
+                    </svg>
+                    Loading...
+                </button>
+                }
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+}
+
+export default NpoModal;
