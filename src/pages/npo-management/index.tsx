84cbbddc4b88892e065d49e46a83e06231ca8d16
jonasgwt <jongoh2000@gmail.com>
2024-02-04 11:21:42 +0800

    Restrict Actions based on user role

--- src/pages/npo-management/index.tsx
+++ src/pages/npo-management/index.tsx
@@ -1,3 +1,4 @@
+import { useSession } from "next-auth/react";
 import { PageLayout } from "../../components/Layout";
 import { LoadingPage } from "../../components/Loading";
 import Pagination from "../../components/Pagination";
@@ -7,6 +8,7 @@ import { api } from "~/utils/api";
 
 const NpoManagement = () => {
   const ENTRIES_PER_PAGE = 10;
+  const { data: session } = useSession();
   const [showModal, setShowModal] = useState(false);
   const [isEdit, setIsEdit] = useState(false);
   const [id, setId] = useState<string | null>(null);
@@ -55,7 +57,9 @@ const NpoManagement = () => {
         </div>
         <div className="m-auto flex w-[90vw] flex-row items-center justify-between text-[2rem] font-bold md:w-[50vw] md:max-w-xl">
           <h1>NPOs</h1>
-          <NewNPOButton onClick={handleCreate} />
+          {session && session.user && session.user.role === "ADMIN" && (
+            <NewNPOButton onClick={handleCreate} />
+          )}
         </div>
         <div className="mt-[10px] flex min-h-screen flex-col items-center gap-[30px]">
           {isLoading ? (
@@ -108,6 +112,7 @@ const NPOCard = ({
   logoLink: string | null;
   onEdit: () => void;
 }) => {
+  const { data: session } = useSession();
   return (
     <div className="relative flex w-[90vw] flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] md:w-[50vw] md:max-w-xl md:flex-row dark:bg-neutral-700">
       <img
@@ -130,26 +135,28 @@ const NPOCard = ({
           <ViewOppButton />
           {webLink && <VisitNPOButton webLink={webLink} />}
         </div>
-        <button
-          type="button"
-          onClick={onEdit}
-          className="absolute right-4 top-4 flex h-[2.375rem] w-[2.375rem] flex-shrink-0 items-center justify-center gap-1 rounded-full border border-transparent bg-yellow-500 text-sm font-semibold text-white hover:bg-yellow-600 disabled:pointer-events-none disabled:opacity-50 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-pencil-square"
-            viewBox="0 0 16 16"
+        {session && session.user && session.user.role === "ADMIN" && (
+          <button
+            type="button"
+            onClick={onEdit}
+            className="absolute right-4 top-4 flex h-[2.375rem] w-[2.375rem] flex-shrink-0 items-center justify-center gap-1 rounded-full border border-transparent bg-yellow-500 text-sm font-semibold text-white hover:bg-yellow-600 disabled:pointer-events-none disabled:opacity-50 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
           >
-            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-            <path
-              fill-rule="evenodd"
-              d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
-            />
-          </svg>
-        </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-pencil-square"
+              viewBox="0 0 16 16"
+            >
+              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+              <path
+                fill-rule="evenodd"
+                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
+              />
+            </svg>
+          </button>
+        )}
       </div>
     </div>
   );
