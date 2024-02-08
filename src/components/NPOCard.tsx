61d27cebadd2e85e21a96f66b9d2f5f35e85c52a
jonasgwt <jongoh2000@gmail.com>
2024-02-08 20:10:36 +0800

    Extract out EditButton

--- src/components/NPOCard.tsx
+++ src/components/NPOCard.tsx
@@ -1,4 +1,5 @@
 import { useSession } from "next-auth/react";
+import EditButton from "./EditButton";
 
 const NPOCard = () => {
   const { data: session } = useSession();
@@ -74,26 +75,7 @@ const NPOCard = () => {
         </svg>
       </a>
       {isAdmin && (
-        <button
-          type="button"
-          onClick={() => console.log("Edit")}
-          className="absolute right-4 top-4 flex h-[2.375rem] w-[2.375rem] flex-shrink-0 items-center justify-center gap-1 rounded-full border border-transparent bg-yellow-500 text-sm font-semibold text-white hover:bg-yellow-600 disabled:pointer-events-none disabled:opacity-50 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-pencil-square"
-            viewBox="0 0 16 16"
-          >
-            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-            <path
-              fill-rule="evenodd"
-              d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
-            />
-          </svg>
-        </button>
+        <EditButton onEdit={() => console.log("Edit button clicked")} />
       )}
     </div>
   );
