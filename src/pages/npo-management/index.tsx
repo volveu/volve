2148abc41e260eb3f6eeb463fdf12696c82e765a
jonasgwt <jongoh2000@gmail.com>
2024-02-08 20:11:24 +0800

    Extract out EditButton

--- src/pages/npo-management/index.tsx
+++ src/pages/npo-management/index.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 import { api } from "~/utils/api";
 import { useRouter } from "next/router";
 import SearchBar from "../../components/SearchBar";
+import EditButton from "../../components/EditButton";
 
 const NpoManagement = () => {
   const ENTRIES_PER_PAGE = 10;
@@ -162,24 +163,7 @@ const NPOCard = ({
         </p>
         {/* {webLink && <VisitNPOButton webLink={webLink} />} */}
         {session && session.user && session.user.role === "ADMIN" && (
-          <button
-            type="button"
-            onClick={onEdit}
-            className="absolute right-4 top-4 flex h-6 w-6 flex-shrink-0 items-center justify-center gap-1 rounded-sm border border-transparent text-sm font-semibold text-white hover:scale-110 disabled:pointer-events-none disabled:opacity-50 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              className="bi bi-pencil-square h-4 w-4"
-              viewBox="0 0 16 16"
-            >
-              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-              <path
-                fill-rule="evenodd"
-                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
-              />
-            </svg>
-          </button>
+          <EditButton onEdit={onEdit} />
         )}
       </div>
     </div>
